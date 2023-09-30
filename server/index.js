f8a3414e144a9bbd38809f33f2a9c4fbbaf27508
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-09-30 10:27:02 +0200

    added server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3001;
+
+app.get("/api/data", async (req, res) => {
+  try {
+    // Replace with your API endpoint
+    const response = await axios.get("https://api.example.com/data");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
