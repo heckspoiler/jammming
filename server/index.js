f6937347e427a08ea6d52810584d8d51e1f2b239
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-09-30 16:13:35 +0200

    backend working

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 const queryString = require("node:querystring");
 const app = express();
-const PORT = 3001;
+const PORT = 8000;
 
 const SPOTIFY_ID = process.env.SPOTIFY_ID;
 const SPOTIFY_KEY = process.env.SPOTIFY_KEY;
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send(
     "<a href='https://accounts.spotify.com/authorize?client_id=" +
       process.env.SPOTIFY_ID +
-      "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Faccount&scope=user-top-read'>Sign in</a>"
+      "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fcallback&scope=user-top-read'>Sign in</a>"
   );
 });
 
@@ -27,8 +27,8 @@ app.get("/callback", async (req, res) => {
       "https://accounts.spotify.com/api/token",
       queryString.stringify({
         grant_type: "authorization_code",
-        code: SPOTIFY_KEY,
-        redirect_uri: "http://localhost:3001/callback",
+        code: req.query.code,
+        redirect_uri: process.env.REDIRECT_URI_DECODED,
       }),
       {
         headers: {
@@ -37,7 +37,19 @@ app.get("/callback", async (req, res) => {
         },
       }
     );
-    console.log(spotifyResponse.data);
+    const accessToken = spotifyResponse.data.access_token;
+    console.log("Access Token:", accessToken);
+
+    const userTopArtistsResponse = await axios.get(
+      "https://api.spotify.com/v1/me/top/artists",
+      {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      }
+    );
+
+    res.send(userTopArtistsResponse.data);
   } catch (error) {
     console.error("Error fetching token from Spotify:", error.response.data);
     res.status(500).send("Error fetching token from Spotify.");
@@ -46,5 +58,5 @@ app.get("/callback", async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(SPOTIFY_ID);
+  console.log(SPOTIFY_ID, process.env.REDIRECT_URI_DECODED);
 });
