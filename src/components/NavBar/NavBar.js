c5e9dece1a22e5469463e0e04692049f34b4f314
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-01 22:25:34 +0200

    looking like a kindergärtner's work

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
-import ArtistPage from "../../pages/ArtistPage/ArtistPage";
 
 const NavBar = () => {
   return (
     <ul className={style.navbar}>
-      <Link to="/">home</Link>
-      <Link to="/artists">artists</Link>
-      <li>Playlists</li>
-      <li>Songs</li>
-      <li>Podcasts</li>
-      <li></li>
+      <Link to="/" className={style.navLink}>
+        home
+      </Link>
+      <Link to="/artists" className={style.navLink}>
+        artists
+      </Link>
+      {/* <li className="nav-link">Playlists</li>
+      <li className="nav-link">Songs</li>
+      <li className="nav-link">Podcasts</li> */}
     </ul>
   );
 };
